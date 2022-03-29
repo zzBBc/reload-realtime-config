9b61ccbc05031f75f6282666536804b4e4949689
vuong1962 <vuongngxuan0109@gmail.com>
2022-03-30 01:56:41 +0700

    Check first time load config

--- src/main/java/com/github/zzbbc/config/RealtimeConfig.java
+++ src/main/java/com/github/zzbbc/config/RealtimeConfig.java
@@ -2,6 +2,8 @@ package com.github.zzbbc.config;
 
 import com.github.zzbbc.config.log.LogConfig;
 import com.github.zzbbc.utils.ConfigUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import io.vertx.config.ConfigRetriever;
 import io.vertx.config.ConfigRetrieverOptions;
 import io.vertx.config.ConfigStoreOptions;
@@ -9,13 +11,23 @@ import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 
 public class RealtimeConfig {
+    Logger LOGGER = LoggerFactory.getLogger(RealtimeConfig.class);
+
     private LogConfig log;
     private JsonObject realtimeConfig;
     private String path;
     private InstanceLoadConfig instance;
+    private boolean startLoad = false;
+    private boolean firstTimeLoaded = false;
 
     public RealtimeConfig(String path, InstanceLoadConfig instance, LogConfig log) {
         this.log = log;
+        this.path = path;
+        this.instance = instance;
+
+    }
+
+    public RealtimeConfig(String path, InstanceLoadConfig instance) {
         this.path = path;
         this.instance = instance;
     }
@@ -31,12 +43,30 @@ public class RealtimeConfig {
         retriever.configStream().exceptionHandler(t -> {
             logError(t);
         }).handler(config -> {
+            startLoad = true;
             log("\n Old config: \n" + realtimeConfig);
             // New configuration
             realtimeConfig = config;
             log("\n New config: \n" + realtimeConfig);
             instance.loadConfig(config);
+
+            firstTimeLoaded = true;
         });
+    }
+
+    public boolean firstTimeLoaded() {
+        if (startLoad) {
+            while (!firstTimeLoaded) {
+                LOGGER.info("Realtime loading...");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                }
+            }
+            LOGGER.info("Realtime config loaded!");
+        }
+
+        return firstTimeLoaded;
     }
 
     public JsonObject getRealtimeConfig() {
